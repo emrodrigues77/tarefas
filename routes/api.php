d198ce1cff4da5a4ad702b1ffbfe9a80ec096559
emrodrigues77 <eduardo@eduardo.rodrigues.nopm.br>
2022-05-24 17:27:47 -0300

    Routes

--- routes/api.php
+++ routes/api.php
@@ -1,19 +1,30 @@
 <?php
 
-use Illuminate\Http\Request;
+use App\Http\Controllers\API\TaskController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Auth\AuthController;
+use App\Http\Controllers\API\UserController;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
+Route::group([
+    'prefix' => 'v1'
+], function () {
+    // rotas de usuário
+    Route::post('login', [AuthController::class, 'login']);
+    Route::post('register', [AuthController::class, 'register']);
+    Route::post('logout', [AuthController::class, 'logout']);
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+    // rotas de tarefas
+    Route::get('/{user}/tasks/all', [UserController::class, 'getAllTasks']);
+    Route::get('/{user}/tasks/finished', [UserController::class, 'getFinishedTasks']);
+    Route::get('/{user}/tasks/unfinished', [UserController::class, 'getUnfinishedTasks']);
+    Route::get('/{user}/tasks/stats', [UserController::class, 'getTasksStats']);
+    Route::post('/{user}/tasks/store', [TaskController::class, 'store']);
+    Route::patch('/{user}/tasks/{task}/update', [TaskController::class, 'update']);
+    Route::patch('/{user}/tasks/{task}/status/{status}', [TaskController::class, 'setStatus']);
+    Route::delete('/{user}/tasks/{task}/destroy', [TaskController::class, 'destroy']);
+
+    // rotas de estatísticas de tarefas
+    Route::get('/{user}/tasks/all/count', [UserController::class, 'getAllTasksCount']);
+    Route::get('/{user}/tasks/finished/count', [UserController::class, 'getFinishedTasksCount']);
+    Route::get('/{user}/tasks/unfinished/count', [UserController::class, 'getUnfinishedTasksCount']);
 });
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\UserController;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -19,4 +21,16 @@ Route::get('/', function () {
 
 Auth::routes();
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+// grupo de rotas protegidas pelo sistema de autenticação
+Route::middleware(['auth'])->group(
+    function () {
+        Route::get('/home', [App\Http\Controllers\Core\HomeController::class, 'index'])->name('home');
+        Route::get('/tasks/create', [App\Http\Controllers\Core\TaskController::class, 'create'])->name('tasks.create');
+        Route::get('/tasks/{task}/edit', [App\Http\Controllers\Core\TaskController::class, 'edit'])->name('tasks.edit');
+        Route::post('/tasks/store', [App\Http\Controllers\Core\TaskController::class, 'store'])->name('tasks.store');
+        Route::patch('/tasks/{task}/update', [App\Http\Controllers\Core\TaskController::class, 'update'])->name('tasks.update');
+        Route::get('/tasks/{task}/destroy', [App\Http\Controllers\Core\TaskController::class, 'destroy'])->name('tasks.destroy');
+        Route::get('/tasks/{task}/status/{status}', [App\Http\Controllers\Core\TaskController::class, 'setStatus'])->name('tasks.status');
+        Route::get('/tasks/{status}', [App\Http\Controllers\Core\TaskController::class, 'showFilteredTasks'])->name('tasks.filtered');
+    }
+);
